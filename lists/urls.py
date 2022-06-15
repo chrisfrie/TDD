9d68858b95aedd33d4a817f9ed49c5efc7cfadfb
Christina <christina.friede5@gmail.com>
2022-06-15 13:53:25 +0200

    Add deploy stage

--- lists/urls.py
+++ lists/urls.py
@@ -20,7 +20,7 @@ from lists import views
 
 urlpatterns = [
     #url(r'^$', views.home_page, name='home'),
-    url(r'^new/$', views.new_list, name='new_list'),
+    url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
